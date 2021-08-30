69006a18df4d0cbad73829f849260b98dc6ca853
Matt McCallum <matt.c.mccallum@gmail.com>
2021-08-30 16:06:21 -0700

    * Moved config construction to a separate object from Config class, and fixed security CLI issues by removing eval statements

--- configipy/__init__.py
+++ configipy/__init__.py
@@ -6,6 +6,7 @@ from .config import *
 from .configurable import *
 from .config_list import *
 from .config_collection import *
+from .config_factory import *
 
 # TODO [matt.c.mccallum 01.05.21]: Remove the two below in favor of the config factory??? - will need to update bambooshoot and pandafeet
 from .helper_funcs import class_for_config
--- configipy/cli/cli.py
+++ configipy/cli/cli.py
@@ -13,6 +13,8 @@ import ruamel.yaml
 # Local imports
 from . import options
 import json
+from configipy import Configurable, config_factory
+from configipy import ConfigFactory
 
 
 @click.group()
@@ -29,9 +31,14 @@ def construct(config, configurable, pymodule):
     Constructs a Config as a yaml file, for a given Configurable in a given module.
     """
     for module in pymodule:
-        from_x_import_all(module)
+        importlib.import_module(module)
     print('\n')
-    cfg = eval(f"{configurable}.CreateConfig(True)")
+    try:
+        configurable = next(x for x in Configurable._all_subclasses() if x.__name__ == configurable)
+    except StopIteration:
+        print(f"Error: No Configurable named {configurable} found (after importing: {pymodule})")
+        return
+    cfg = ConfigFactory(True).CreateConfig(configurable)
     if config != None and len(config) > 0:
         with open(config, 'w') as f:
             ruamel.yaml.YAML().dump(cfg, f)
@@ -47,8 +54,13 @@ def options(configurable, pymodule):
     Prints out all sub configurables for a given parent configurable.
     """
     for module in pymodule:
-        from_x_import_all(module)
-    subclss = eval(f"{configurable}._all_subclasses()")
+        importlib.import_module(module)
+    try:
+        configurable = next(x for x in Configurable._all_subclasses() if x.__name__ == configurable)
+    except StopIteration:
+        print(f"Error: No Configurable named {configurable} found (after importing: {pymodule})")
+        return
+    subclss = configurable._all_subclasses()
     # TODO [matt.c.mccallum 02.03.21]: If there's no subclasses just print out the base class
     names = [sub.__name__ for sub in subclss]
     docs = [sub.__PARAM_DOCS__ for sub in subclss]
@@ -57,26 +69,7 @@ def options(configurable, pymodule):
         result += f'\n#\n# {nm}\n#\n'
         this_doc = dc.rstrip() + '\n\n'
         result += textwrap.indent(this_doc, '###', lambda x: True)
-        # result += '\n'
     print(result)
-
-
-def from_x_import_all(x):
-    """
-    """
-
-    # get a handle on the module
-    mdl = importlib.import_module(x)
-
-    # is there an __all__?  if so respect it
-    if "__all__" in mdl.__dict__:
-        names = mdl.__dict__["__all__"]
-    else:
-        # otherwise we import all names that don't begin with _
-        names = [name for name in mdl.__dict__ if not name.startswith("_")]
-
-    # now drag them in
-    globals().update({k: getattr(mdl, k) for k in names})
 
 
 if __name__=='__main__':
